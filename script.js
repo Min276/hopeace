7ce3f49a882986bd27b1d1df274de12fa2f266ec
Min Khant Kyaw <minkhantkyawttu@gmail.com>
2022-01-15 23:55:50 +0630

    dynamic posts

--- script.js
+++ script.js
@@ -1,3 +1,64 @@
+const popularPosts = [
+    {
+        title : "Online Learning Strategies",
+        text  : "We'll explain about the topic, and give you some tips and tricks.",
+        image : "images/image.jpg",
+        alter : "studying lessons"
+    },
+    {
+        title : "Online Learning Strategies",
+        text  : "We'll explain about the topic, and give you some tips and tricks.",
+        image : "images/image.jpg",
+        alter : "studying lessons"
+    },
+    {
+        title : "Online Learning Strategies",
+        text  : "We'll explain about the topic, and give you some tips and tricks.",
+        image : "images/image.jpg",
+        alter : "studying lessons"
+    },
+    {
+        title : "Online Learning Strategies",
+        text  : "We'll explain about the topic, and give you some tips and tricks.",
+        image : "images/image.jpg",
+        alter : "studying lessons"
+    },
+    {
+        title : "Online Learning Strategies",
+        text  : "We'll explain about the topic, and give you some tips and tricks.",
+        image : "images/image.jpg",
+        alter : "studying lessons"
+    },
+    {
+        title : "Online Learning Strategies",
+        text  : "We'll explain about the topic, and give you some tips and tricks.",
+        image : "images/image.jpg",
+        alter : "studying lessons"
+    }
+]
+
+let showPosts = document.getElementById("showPosts");
+let postDatas = "";
+
+const loopPosts = popularPosts.forEach(({title,text,image,alter}) => {
+    
+      postDatas += `
+            <div class="ms-2 me-2 mb-3">
+                <div class="card">
+                   <img src="${image}" alt="${alter}">
+                    <div class="card-body">
+                       <h3 class="card-title">${title}</h3>
+                       <p class="card-text">${text}</p>
+                       <a href="#" class="btn postButton">Read More</a>
+                    </div>
+                </div>
+            </div> `
+     //show the cards in the HTML
+     showPosts.innerHTML = postDatas;
+    
+});
+document.addEventListener("DOMContentLoaded", loopPosts);
+
 $('.owl-carousel').owlCarousel({
     autoplay: true,
     autoplayhoverpause: true,
@@ -18,3 +79,4 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
