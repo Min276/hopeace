c21f0bbb51847699f82b4716614f2b357b8205d4
Min Khant Kyaw <minkhantkyawttu@gmail.com>
2022-01-23 02:30:44 +0630

    adding effects

--- script.js
+++ script.js
@@ -3,44 +3,56 @@ const popularPosts = [
         title : "Online Learning Strategies",
         text  : "We'll explain about the topic, and give you some tips and tricks.",
         image : "images/image.jpg",
-        alter : "studying lessons"
+        alter : "studying lessons",
+        author: "Nyein Su",
+        time  : "1 min read"
     },
     {
         title : "Online Learning Strategies",
         text  : "We'll explain about the topic, and give you some tips and tricks.",
         image : "images/image.jpg",
-        alter : "studying lessons"
+        alter : "studying lessons",
+        author: "Ye Myat",
+        time  : "2 mins read"
     },
     {
         title : "Online Learning Strategies",
         text  : "We'll explain about the topic, and give you some tips and tricks.",
         image : "images/image.jpg",
-        alter : "studying lessons"
+        alter : "studying lessons",
+        author: "Myat Min",
+        time  : "1.5 min read"
     },
     {
         title : "Online Learning Strategies",
         text  : "We'll explain about the topic, and give you some tips and tricks.",
         image : "images/image.jpg",
-        alter : "studying lessons"
+        alter : "studying lessons",
+        author: "Su Myat",
+        time  : "1 min read"
     },
     {
         title : "Online Learning Strategies",
         text  : "We'll explain about the topic, and give you some tips and tricks.",
         image : "images/image.jpg",
-        alter : "studying lessons"
+        alter : "studying lessons",
+        author: "Chaw Su",
+        time  : "2 mins read"
     },
     {
         title : "Online Learning Strategies",
         text  : "We'll explain about the topic, and give you some tips and tricks.",
         image : "images/image.jpg",
-        alter : "studying lessons"
+        alter : "studying lessons",
+        author: "Phone Myat",
+        time  : "3 mins read"
     }
 ]
 
 let showPosts = document.getElementById("showPosts");
 let postDatas = "";
 
- popularPosts.forEach(({title,text,image,alter}) => {
+ popularPosts.forEach(({title,text,image,alter,time}) => {
 
       postDatas += `
             <div class="ms-2 me-2 mb-3">
@@ -49,7 +61,11 @@ let postDatas = "";
                     <div class="card-body">
                        <h3 class="card-title">${title}</h3>
                        <p class="card-text">${text}</p>
-                       <a href="#" class="btn postButton">Read More</a>
+                       <div class="group">
+                         <a href="#" class="btn postButton">Read More</a>
+                         <a href="#" class="role-icon"><i class="far fa-clock" style="font-size: 25px;"></i>
+                         <span class="role">&nbsp;${time}</span></a>
+                       </div>
                     </div>
                 </div>
             </div> `
@@ -79,4 +95,6 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+
 
